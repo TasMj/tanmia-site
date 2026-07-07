4689f72b54fae12e2201dd1d7f3ba096ee88a1be
Tasnim <tasnim@mot.tech>
2026-07-07 16:19:53 +0200

    update service email js id

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@
 export const CALENDLY_URL = 'https://calendly.com/tasnim-tanmia/30min?hide_event_type_details=1&hide_gdpr_banner=1'
 
 // 2. EMAILJS (pour le formulaire de contact)
-export const EMAILJS_SERVICE_ID = 'service_2igmoks'
+export const EMAILJS_SERVICE_ID = 'service_0hiuqtg'
 export const EMAILJS_TEMPLATE_ID = 'template_wrshbnn'
 export const EMAILJS_PUBLIC_KEY = 'gGnAUGByPEQZTOIpI'
 
